06b66ba98fc0b60eedaf619c21091079a8f006be
Justas <justkolyta@gmail.com>
2025-01-26 14:55:42 +0200

    human to dog years calculator

---
+++ script.js
@@ -0,0 +1,27 @@
+//program that counts dog years
+// my age variable
+const myAge=21;
+//early years variable
+ let earlyYears=2;
+ //early dog years counted
+earlyYears=2*10.5;
+ console.log(earlyYears)
+ 
+ // my age subtracted by 2
+let laterYears=`${myAge}`-2;
+console.log(laterYears);
+
+//laterYears of dog age by mutiplication assignment operator
+laterYears*=4;
+console.log(laterYears);
+
+//two variables result
+const myAgeInDogYears=earlyYears+laterYears;
+
+let myName="Justas".toLowerCase();
+
+//my name as a string turned to lowercase 
+
+//not all dogs age the same
+console.log("my name is "+myName+" I am "+myAge+" years old in human years which is "+myAgeInDogYears+" years old in dog years" )
+// i could be senior dog
